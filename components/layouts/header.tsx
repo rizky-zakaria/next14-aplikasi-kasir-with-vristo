b6318ee613142ef1cd2aa214d81fcc9466511ce8
Rizky Zakaria <rizky.zakaria78@gmail.com>
2025-01-05 00:55:00 +0800

    first commit

---
+++ components/layouts/header.tsx
@@ -0,0 +1,326 @@
+'use client';
+import Dropdown from '@/components/dropdown';
+import IconLaptop from '@/components/icon/icon-laptop';
+import IconLogout from '@/components/icon/icon-logout';
+import IconMenu from '@/components/icon/icon-menu';
+import IconMoon from '@/components/icon/icon-moon';
+import IconSearch from '@/components/icon/icon-search';
+import IconSun from '@/components/icon/icon-sun';
+import IconUser from '@/components/icon/icon-user';
+import IconXCircle from '@/components/icon/icon-x-circle';
+import IconMenuDashboard from '@/components/icon/menu/icon-menu-dashboard';
+import { getTranslation } from '@/i18n';
+import { IRootState } from '@/store';
+import { toggleRTL, toggleSidebar, toggleTheme } from '@/store/themeConfigSlice';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import IconMenuDocumentation from '../icon/menu/icon-menu-documentation';
+import IconMenuDragAndDrop from '../icon/menu/icon-menu-drag-and-drop';
+import IconMenuInvoice from '../icon/menu/icon-menu-invoice';
+import IconMenuWidgets from '../icon/menu/icon-menu-widgets';
+
+const Header = () => {
+    const pathname = usePathname();
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const { t, i18n } = getTranslation();
+
+    useEffect(() => {
+        const selector = document.querySelector('ul.horizontal-menu a[href="' + window.location.pathname + '"]');
+        if (selector) {
+            const all: any = document.querySelectorAll('ul.horizontal-menu .nav-link.active');
+            for (let i = 0; i < all.length; i++) {
+                all[0]?.classList.remove('active');
+            }
+
+            let allLinks = document.querySelectorAll('ul.horizontal-menu a.active');
+            for (let i = 0; i < allLinks.length; i++) {
+                const element = allLinks[i];
+                element?.classList.remove('active');
+            }
+            selector?.classList.add('active');
+
+            const ul: any = selector.closest('ul.sub-menu');
+            if (ul) {
+                let ele: any = ul.closest('li.menu').querySelectorAll('.nav-link');
+                if (ele) {
+                    ele = ele[0];
+                    setTimeout(() => {
+                        ele?.classList.add('active');
+                    });
+                }
+            }
+        }
+    }, [pathname]);
+
+    const isRtl = useSelector((state: IRootState) => state.themeConfig.rtlClass) === 'rtl';
+
+    const themeConfig = useSelector((state: IRootState) => state.themeConfig);
+    const setLocale = (flag: string) => {
+        if (flag.toLowerCase() === 'ae') {
+            dispatch(toggleRTL('rtl'));
+        } else {
+            dispatch(toggleRTL('ltr'));
+        }
+        router.refresh();
+    };
+
+    function createMarkup(messages: any) {
+        return { __html: messages };
+    }
+    const [messages, setMessages] = useState([
+        {
+            id: 1,
+            image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-success-light dark:bg-success text-success dark:text-success-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg></span>',
+            title: 'Congratulations!',
+            message: 'Your OS has been updated.',
+            time: '1hr',
+        },
+        {
+            id: 2,
+            image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-info-light dark:bg-info text-info dark:text-info-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></span>',
+            title: 'Did you know?',
+            message: 'You can switch between artboards.',
+            time: '2hr',
+        },
+        {
+            id: 3,
+            image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-danger-light dark:bg-danger text-danger dark:text-danger-light"> <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span>',
+            title: 'Something went wrong!',
+            message: 'Send Reposrt',
+            time: '2days',
+        },
+        {
+            id: 4,
+            image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-warning-light dark:bg-warning text-warning dark:text-warning-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">    <circle cx="12" cy="12" r="10"></circle>    <line x1="12" y1="8" x2="12" y2="12"></line>    <line x1="12" y1="16" x2="12.01" y2="16"></line></svg></span>',
+            title: 'Warning',
+            message: 'Your password strength is low.',
+            time: '5days',
+        },
+    ]);
+
+    const removeMessage = (value: number) => {
+        setMessages(messages.filter((user) => user.id !== value));
+    };
+
+    const [notifications, setNotifications] = useState([
+        {
+            id: 1,
+            profile: 'user-profile.jpeg',
+            message: '<strong class="text-sm mr-1">John Doe</strong>invite you to <strong>Prototyping</strong>',
+            time: '45 min ago',
+        },
+        {
+            id: 2,
+            profile: 'profile-34.jpeg',
+            message: '<strong class="text-sm mr-1">Adam Nolan</strong>mentioned you to <strong>UX Basics</strong>',
+            time: '9h Ago',
+        },
+        {
+            id: 3,
+            profile: 'profile-16.jpeg',
+            message: '<strong class="text-sm mr-1">Anna Morgan</strong>Upload a file',
+            time: '9h Ago',
+        },
+    ]);
+
+    const removeNotification = (value: number) => {
+        setNotifications(notifications.filter((user) => user.id !== value));
+    };
+
+    const [search, setSearch] = useState(false);
+
+    return (
+        <header className={`z-40 ${themeConfig.semidark && themeConfig.menu === 'horizontal' ? 'dark' : ''}`}>
+            <div className="shadow-sm">
+                <div className="relative flex w-full items-center bg-white px-5 py-2.5 dark:bg-black">
+                    <div className="horizontal-logo flex items-center justify-between ltr:mr-2 rtl:ml-2 lg:hidden">
+                        <Link href="/" className="main-logo flex shrink-0 items-center">
+                            <img className="inline w-8 ltr:-ml-1 rtl:-mr-1" src="/assets/images/logo.svg" alt="logo" />
+                            <span className="hidden align-middle text-2xl  font-semibold  transition-all duration-300 ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light md:inline">VRISTO</span>
+                        </Link>
+                        <button
+                            type="button"
+                            className="collapse-icon flex flex-none rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary ltr:ml-2 rtl:mr-2 dark:bg-dark/40 dark:text-[#d0d2d6] dark:hover:bg-dark/60 dark:hover:text-primary lg:hidden"
+                            onClick={() => dispatch(toggleSidebar())}
+                        >
+                            <IconMenu className="h-5 w-5" />
+                        </button>
+                    </div>
+
+                    <div className="hidden ltr:mr-2 rtl:ml-2 sm:block">
+                        {/* <ul className="flex items-center space-x-2 rtl:space-x-reverse dark:text-[#d0d2d6]">
+                            <li>
+                                <Link href="/apps/calendar" className="block rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60">
+                                    <IconCalendar />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/apps/todolist" className="block rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60">
+                                    <IconEdit />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/apps/chat" className="block rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60">
+                                    <IconChatNotification />
+                                </Link>
+                            </li>
+                        </ul> */}
+                    </div>
+                    <div className="flex items-center space-x-1.5 ltr:ml-auto rtl:mr-auto rtl:space-x-reverse dark:text-[#d0d2d6] sm:flex-1 ltr:sm:ml-0 sm:rtl:mr-0 lg:space-x-2">
+                        <div className="sm:ltr:mr-auto sm:rtl:ml-auto">
+                            <form
+                                className={`${search && '!block'} absolute inset-x-0 top-1/2 z-10 mx-4 hidden -translate-y-1/2 sm:relative sm:top-0 sm:mx-0 sm:block sm:translate-y-0`}
+                                onSubmit={() => setSearch(false)}
+                            >
+                                <div className="relative">
+                                    <input
+                                        type="text"
+                                        className="peer form-input bg-gray-100 placeholder:tracking-widest ltr:pl-9 ltr:pr-9 rtl:pl-9 rtl:pr-9 sm:bg-transparent ltr:sm:pr-4 rtl:sm:pl-4"
+                                        placeholder="Tanya AI"
+                                    />
+                                    <button type="button" className="absolute inset-0 h-9 w-9 appearance-none peer-focus:text-primary ltr:right-auto rtl:left-auto">
+                                        <IconSearch className="mx-auto" />
+                                    </button>
+                                    <button type="button" className="absolute top-1/2 block -translate-y-1/2 hover:opacity-80 ltr:right-2 rtl:left-2 sm:hidden" onClick={() => setSearch(false)}>
+                                        <IconXCircle />
+                                    </button>
+                                </div>
+                            </form>
+                            <button
+                                type="button"
+                                onClick={() => setSearch(!search)}
+                                className="search_btn rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 dark:bg-dark/40 dark:hover:bg-dark/60 sm:hidden"
+                            >
+                                <IconSearch className="mx-auto h-4.5 w-4.5 dark:text-[#d0d2d6]" />
+                            </button>
+                        </div>
+                        <div>
+                            {themeConfig.theme === 'light' ? (
+                                <button
+                                    className={`${
+                                        themeConfig.theme === 'light' &&
+                                        'flex items-center rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60'
+                                    }`}
+                                    onClick={() => dispatch(toggleTheme('dark'))}
+                                >
+                                    <IconSun />
+                                </button>
+                            ) : (
+                                ''
+                            )}
+                            {themeConfig.theme === 'dark' && (
+                                <button
+                                    className={`${
+                                        themeConfig.theme === 'dark' &&
+                                        'flex items-center rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60'
+                                    }`}
+                                    onClick={() => dispatch(toggleTheme('system'))}
+                                >
+                                    <IconMoon />
+                                </button>
+                            )}
+                            {themeConfig.theme === 'system' && (
+                                <button
+                                    className={`${
+                                        themeConfig.theme === 'system' &&
+                                        'flex items-center rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60'
+                                    }`}
+                                    onClick={() => dispatch(toggleTheme('light'))}
+                                >
+                                    <IconLaptop />
+                                </button>
+                            )}
+                        </div>
+
+                        <div className="dropdown flex shrink-0">
+                            <Dropdown
+                                offset={[0, 8]}
+                                placement={`${isRtl ? 'bottom-start' : 'bottom-end'}`}
+                                btnClassName="relative group block"
+                                button={<img className="h-9 w-9 rounded-full object-cover saturate-50 group-hover:saturate-100" src="/assets/images/user-profile.jpeg" alt="userProfile" />}
+                            >
+                                <ul className="w-[230px] !py-0 font-semibold text-dark dark:text-white-dark dark:text-white-light/90">
+                                    <li>
+                                        <div className="flex items-center px-4 py-4">
+                                            <img className="h-10 w-10 rounded-md object-cover" src="/assets/images/user-profile.jpeg" alt="userProfile" />
+                                            <div className="truncate ltr:pl-4 rtl:pr-4">
+                                                <h4 className="text-base">
+                                                    John Doe
+                                                    <span className="rounded bg-success-light px-1 text-xs text-success ltr:ml-2 rtl:ml-2">Pro</span>
+                                                </h4>
+                                                <button type="button" className="text-black/60 hover:text-primary dark:text-dark-light/60 dark:hover:text-white">
+                                                    johndoe@gmail.com
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <Link href="/users/profile" className="dark:hover:text-white">
+                                            <IconUser className="h-4.5 w-4.5 shrink-0 ltr:mr-2 rtl:ml-2" />
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li className="border-t border-white-light dark:border-white-light/10">
+                                        <Link href="/auth/boxed-signin" className="!py-3 text-danger">
+                                            <IconLogout className="h-4.5 w-4.5 shrink-0 rotate-90 ltr:mr-2 rtl:ml-2" />
+                                            Sign Out
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </Dropdown>
+                        </div>
+                    </div>
+                </div>
+
+                {/* horizontal menu */}
+                <ul className="horizontal-menu hidden border-t border-[#ebedf2] bg-white px-6 py-1.5 font-semibold text-black rtl:space-x-reverse dark:border-[#191e3a] dark:bg-black dark:text-white-dark lg:space-x-1.5 xl:space-x-8">
+                    <li className="menu nav-item relative">
+                        <Link href="/" className="nav-link">
+                            <div className="flex items-center">
+                                <IconMenuDashboard className="shrink-0" />
+                                <span className="px-1">{t('dashboard')}</span>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="menu nav-item relative">
+                        <Link href="/produk" className="nav-link">
+                            <div className="flex items-center">
+                                <IconMenuWidgets className="shrink-0" />
+                                <span className="px-1">{t('Produk')}</span>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="menu nav-item relative">
+                        <Link href="/transaksi" className="nav-link">
+                            <div className="flex items-center">
+                                <IconMenuInvoice className="shrink-0" />
+                                <span className="px-1">{t('Transaksi')}</span>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="menu nav-item relative">
+                        <Link href="/pengeluaran" className="nav-link">
+                            <div className="flex items-center">
+                                <IconMenuDragAndDrop className="shrink-0" />
+                                <span className="px-1">{t('Pengeluaran')}</span>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="menu nav-item relative">
+                        <Link href="/laporan" className="nav-link">
+                            <div className="flex items-center">
+                                <IconMenuDocumentation className="shrink-0" />
+                                <span className="px-1">{t('Laporan')}</span>
+                            </div>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ components/layouts/sidebar.tsx
@@ -0,0 +1,139 @@
+'use client';
+import IconCaretsDown from '@/components/icon/icon-carets-down';
+import IconMenuDashboard from '@/components/icon/menu/icon-menu-dashboard';
+import { getTranslation } from '@/i18n';
+import { IRootState } from '@/store';
+import { toggleSidebar } from '@/store/themeConfigSlice';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { useDispatch, useSelector } from 'react-redux';
+import IconMenuDocumentation from '../icon/menu/icon-menu-documentation';
+import IconMenuDragAndDrop from '../icon/menu/icon-menu-drag-and-drop';
+import IconMenuInvoice from '../icon/menu/icon-menu-invoice';
+import IconMenuWidgets from '../icon/menu/icon-menu-widgets';
+
+const Sidebar = () => {
+    const dispatch = useDispatch();
+    const { t } = getTranslation();
+    const pathname = usePathname();
+    const [currentMenu, setCurrentMenu] = useState<string>('');
+    const [errorSubMenu, setErrorSubMenu] = useState(false);
+    const themeConfig = useSelector((state: IRootState) => state.themeConfig);
+    const semidark = useSelector((state: IRootState) => state.themeConfig.semidark);
+    const toggleMenu = (value: string) => {
+        setCurrentMenu((oldValue) => {
+            return oldValue === value ? '' : value;
+        });
+    };
+
+    useEffect(() => {
+        const selector = document.querySelector('.sidebar ul a[href="' + window.location.pathname + '"]');
+        if (selector) {
+            selector.classList.add('active');
+            const ul: any = selector.closest('ul.sub-menu');
+            if (ul) {
+                let ele: any = ul.closest('li.menu').querySelectorAll('.nav-link') || [];
+                if (ele.length) {
+                    ele = ele[0];
+                    setTimeout(() => {
+                        ele.click();
+                    });
+                }
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        setActiveRoute();
+        if (window.innerWidth < 1024 && themeConfig.sidebar) {
+            dispatch(toggleSidebar());
+        }
+    }, [pathname]);
+
+    const setActiveRoute = () => {
+        let allLinks = document.querySelectorAll('.sidebar ul a.active');
+        for (let i = 0; i < allLinks.length; i++) {
+            const element = allLinks[i];
+            element?.classList.remove('active');
+        }
+        const selector = document.querySelector('.sidebar ul a[href="' + window.location.pathname + '"]');
+        selector?.classList.add('active');
+    };
+
+    return (
+        <div className={semidark ? 'dark' : ''}>
+            <nav
+                className={`sidebar fixed bottom-0 top-0 z-50 h-full min-h-screen w-[260px] shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] transition-all duration-300 ${semidark ? 'text-white-dark' : ''}`}
+            >
+                <div className="h-full bg-white dark:bg-black">
+                    <div className="flex items-center justify-between px-4 py-3">
+                        <Link href="/" className="main-logo flex shrink-0 items-center">
+                            <img className="ml-[5px] w-8 flex-none" src="/assets/images/logo.svg" alt="logo" />
+                            <span className="align-middle text-2xl font-semibold ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light lg:inline">VRISTO</span>
+                        </Link>
+
+                        <button
+                            type="button"
+                            className="collapse-icon flex h-8 w-8 items-center rounded-full transition duration-300 hover:bg-gray-500/10 rtl:rotate-180 dark:text-white-light dark:hover:bg-dark-light/10"
+                            onClick={() => dispatch(toggleSidebar())}
+                        >
+                            <IconCaretsDown className="m-auto rotate-90" />
+                        </button>
+                    </div>
+                    <PerfectScrollbar className="relative h-[calc(100vh-80px)]">
+                        <ul className="relative space-y-0.5 p-4 py-0 font-semibold">
+                            <li className="nav-item">
+                                <ul>
+                                    <li className="nav-item">
+                                        <Link href="/" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuDashboard className="shrink-0 group-hover:!text-primary" />
+                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('dashboard')}</span>
+                                            </div>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link href="/produk" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuWidgets className="shrink-0 group-hover:!text-primary" />
+                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Produk')}</span>
+                                            </div>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link href="/transaksi" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuInvoice className="shrink-0 group-hover:!text-primary" />
+                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Transaksi')}</span>
+                                            </div>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link href="/pengeluaran" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuDragAndDrop className="shrink-0 group-hover:!text-primary" />
+                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Pengeluaran')}</span>
+                                            </div>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link href="/laporan" className="group">
+                                            <div className="flex items-center">
+                                                <IconMenuDocumentation className="shrink-0 group-hover:!text-primary" />
+                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Laporan')}</span>
+                                            </div>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </PerfectScrollbar>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Sidebar;
